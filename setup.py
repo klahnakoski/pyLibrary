57929fd154f6f2ca670c2da64ee34e789cfff87a
Kyle Lahnakoski <klahnakoski@mozilla.com>
2015-02-22 09:22:51 -0500

    pypi version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_packages(where='.', lib_prefix='', exclude=()):
 
 setup(
     name='pyLibrary',
-    version="0.4.15007",
+    version="0.4.15053",
     description='Library of Wonderful Things',
     long_description=long_desc,
     author='Kyle Lahnakoski',
