d3b6987700c0768e2c987ffc2db32d990fe1a43e
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-01-29 00:39:18 -0500

    move

--- mo_threads/signal.py
+++ mo_threads/signal.py
@@ -16,6 +16,7 @@ from __future__ import division
 from __future__ import unicode_literals
 
 from thread import allocate_lock as _allocate_lock
+from thread import get_ident as _get_ident
 
 _Log = None
 DEBUG = False
