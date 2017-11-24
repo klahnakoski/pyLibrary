d1046af5fb4a729998ba41a4e1269eecbe9ff430
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-11-24 16:38:24 -0500

    comments

--- mo_future/__init__.py
+++ mo_future/__init__.py
@@ -1,5 +1,17 @@
-import sys
+# encoding: utf-8
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Author: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+import sys
 
 PY3 = sys.version_info[0] == 3
 PY2 = sys.version_info[0] == 2
